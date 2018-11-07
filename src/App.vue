3bd3ed45f9820463c4d66fde93daec725b096f2a
eking <eking@gmail.com>
2018-11-07 20:06:20 +0800

    Optimized websit framwork.

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,25 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Header
   }
 }
 </script>
 
 <style>
+html, body, div, p, ul, li, h1, h2, h3, a, span {
+  margin: 0;
+  padding: 0;
+}
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,42 @@
+<template>
+<div class="header">
+    <a href="###" class="logo">潘多拉</a>
+    <h1 class="slogen">{{ msg }}</h1>
+</div>
+</template>
+
+<script>
+export default {
+  name: "Header",
+  props: {
+    msg: String
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.header {
+  height: 150px;
+  padding: 0 50px;
+  background-color: antiquewhite;
+}
+.header::after {
+  content: "";
+  clear: both;
+}
+.logo {
+  width: 200px;
+  float: left;
+  height: 150px;
+  background: url("../assets/logo.png") center center no-repeat;
+  background-size:100% auto;
+  text-indent: -999999em;
+}
+.slogen {
+    line-height: 150px;
+    float: left;
+    margin-left: 50px;
+}
+
+</style>
