79723bf021b6ee1dac5fc97e1025eb81a2924f92
eking <ekingwee@gmail.com>
2018-11-07 22:41:31 +0800

    Added menu.

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,70 @@
 <template>
   <div id="app">
-    <Header msg="Welcome to Your Vue.js App"/>
+    <Header msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
 <script>
-import Header from './components/Header.vue'
+import Header from "./components/Header.vue";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
     Header
   }
-}
+};
 </script>
 
 <style>
-html, body, div, p, ul, li, h1, h2, h3, a, span {
+html,
+body,
+div,
+p,
+ul,
+li,
+h1,
+h2,
+h3,
+a,
+span {
+  font-family: "Microsoft YaHei";
   margin: 0;
   padding: 0;
 }
-#app {}
+a {
+  text-decoration: none;
+}
+#app {
+}
+
+@-webkit-keyframes 'menu1' {
+  0% {
+    -webkit-transform: scale(1, 1);
+    -webkit-transform-origin: 40% 40%;
+  }
+  50% {
+    -webkit-transform: scale(1.12, 1.4);
+    -webkit-transform-origin: 40% 40%;
+  }
+  100% {
+    -webkit-transform: scale(1, 1);
+    -webkit-transform-origin: 40% 40%;
+  }
+}
+
+@-webkit-keyframes 'menu2' {
+  0% {
+    -webkit-transform: scale(1, 1);
+    -webkit-transform-origin: 40% 60%;
+  }
+  50% {
+    -webkit-transform: scale(1.12, 1.4);
+    -webkit-transform-origin: 40% 60%;
+  }
+  100% {
+    -webkit-transform: scale(1, 1);
+    -webkit-transform-origin: 40% 60%;
+  }
+}
+
 </style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,13 @@
 <template>
 <div class="header">
     <a href="###" class="logo">潘多拉</a>
+    <div class="menu">
+        <a href="#/jobList/0-1227-10002"><span>技术</span></a>
+        <a href="#/jobList/0-1227-37850530"><span>产品</span></a>
+        <a href="#/jobList/0-1227-73491303"><span>用户设计</span></a>
+        <a href="#/jobList/0-1227-73491301"><span>管理支持</span></a>
+    </div>
+
     <h1 class="slogen">{{ msg }}</h1>
 </div>
 </template>
@@ -37,6 +44,46 @@ export default {
     line-height: 150px;
     float: left;
     margin-left: 50px;
+}
+
+.menu {
+  float: left;
+  font-size: 16px;
+}
+
+.menu a {
+  color: #DBB960;
+  float: left;
+  line-height: 24px;
+  height: 24px;
+  border: 1px solid rgba(219, 185, 96, 0.33);
+  margin: 10px 0 10px 10px;
+  padding: 0 20px;
+  position: relative;
+}
+
+.menu span::after, .menu span::before {
+  content: "";
+  position: absolute;
+  top: -1px;
+  left: -3px;
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+  border: 1px solid #DBB960;
+  opacity: 0;
+}
+
+.menu a:hover span::after, .menu a:hover span::before {
+  opacity: 0.2;
+}
+
+.menu a:hover span::after {
+  -webkit-animation: "menu1" 0.5s linear;
+}
+
+.menu a:hover span::before {
+  -webkit-animation: "menu2" 0.75s linear;
 }
 
 </style>
